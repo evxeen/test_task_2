a4b5add4af0459920d7ae60d0b8a3bad1f3b8e69
evxeen <evxeen@gmail.com>
2022-02-14 20:31:47 +0300

    refactor: refactoring everything))))

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import './App.css';
 import {useState} from "react";
 
 import {Table} from "./components/Table";
+import {Pizza} from "./components/Pizza";
 
 import {CURRENCY_API, DIETS_API, GUESTS_API, ORDER_API} from "./config";
-import {getVegans} from "./helpers";
+import {changeStrings, getData, getPizza} from "./helpers";
 
 function App() {
   const [message, setMessage] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetched = async () => {
-    const guestsResponse = await fetch(GUESTS_API);
-    const guestsData = await guestsResponse.json();
-    const dietsResponse = await fetch(`${DIETS_API}${getVegans(guestsData)}`);
-    const dietsData = await dietsResponse.json();
-
-    const whoEats =  guestsData.party.filter(eat => eat.eatsPizza).length;
-    const checkoutVegans = dietsData.diet.filter(vegan => vegan.isVegan).length;
-    const percentVegans = Math.floor((checkoutVegans / dietsData.diet.length) * 100);
+    const { party } = await getData(GUESTS_API);
+    const { diet } = await getData(DIETS_API, changeStrings(party));
 
     const [order, currency] = await Promise.all(
-        [fetch(`${ORDER_API}${percentVegans > 51 ? 'vegan' : 'meat'}/${whoEats}`).then(res => res.json()),
-      fetch(CURRENCY_API).then(res => res.json())]);
+        [getData(ORDER_API, getPizza(diet)),
+          getData(CURRENCY_API)]);
 
     setMessage(false);
     setIsLoading(false);
@@ -36,7 +31,8 @@ function App() {
     <div className="App">
       <button className='load' onClick={showLoading}>Loading</button>
       {message ? <h2>Load...</h2> : null}
-      {isLoading ? null: <Table /> }
+      {isLoading ? null: <Table />}
+      <Pizza />
     </div>
   );
 }
--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,25 @@
-export const getVegans = (data) => {
+export const changeStrings = (party) => {
   let result = [];
-  data.party.forEach((person) => {
+  party.forEach((person) => {
     result.push(`${person.name.replace(' ' , '%20')}`);
   })
   return result.join();
 }
 
+export const getData = (api, str = '') => {
+   return fetch(`${api}${str}`).then(res => res.json());
+}
+
+export const getPizza = (data) => {
+  const vegans = data.filter(person => person.isVegan).length;
+
+  function getPercent() {
+    const percent = Math.floor((vegans / data.length) * 100);
+    if (percent > 51) {
+      return 'vegan'
+    }
+    return 'meat'
+  }
+
+  return `${getPercent()}/${vegans}`;
+}
